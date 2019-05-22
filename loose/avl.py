21e0a8c0e000b0f88f1acff1a53cf1cb6fe8b98e
Danilo César R G de Medeiros <danilocrgm@gmail.com>
2019-05-22 10:44:12 -0300

    Árvore AVL adicionada

---
+++ loose/avl.py
@@ -0,0 +1,89 @@
+class avlTree():
+
+	def __init__(self,data):
+		self.data = data
+		self.setaFilhos(None,None)
+
+
+	def setaFilhos(self,esquerda,direita):
+
+		self.esquerda = esquerda
+		self.direita = direita
+
+	def balanco(self):
+
+		prof_esq = 0
+		if self.esquerda:
+			prof_esq = self.esquerda.profundidade()
+		prof_dir = 0
+
+		if self.direita:
+			prof_dir = self.direita.profundidade()
+
+		return prof_esq - prof_dir
+
+	def profundidade(self):
+
+		prof_esq = 0
+
+		if self.esquerda:
+			prof_esq = self.esquerda.profundidade()
+		prof_dir = 0
+		if self.direita:
+			prof_dir = self.direita.profundidade()
+
+		return 1 + max(prof_esq, prof_dir)
+
+	def rotacaoEsquerda(self):
+		try:
+			self.data, self.direita.data = self.direita.data, self.data
+			old_esquerda = self.esquerda
+			self.setaFilhos(self.direita, self.direita.direita)
+			self.esquerda.setaFilhos(old_esquerda,self.esquerda.esquerda)
+		except AttributeError:
+			pass
+
+	def rotacaoDireita(self):
+		try:
+			self.data, self.esquerda.data = self.esuqerda.data, self.data
+			old_direita = self.direita
+			self.setaFilhos(self.esquerda.esquerda,self.esquerda)
+			self.direita.setaFilhos(self.direita.direita,old_direita)
+		except AttributeError:
+			pass
+
+	def rotacaoEsquerdaDireita(self):
+
+		self.esquerda.rotacaoEsquerda()
+		self.rotacaoEsquerda()
+
+	def rotacaoDireitaEsquerda(self):
+		self.direita.rotacaoDireita()
+		self.rotacaoEsquerda()
+
+	def executaBalanco(self):
+		bal = self.balanco()
+
+		if bal > 1:
+			if self.esquerda.balanco() > 0:
+				self.rotacaoDireita()
+			else:
+				self.rotacaoEsquerdaDireita()
+		elif bal < -1:
+			if self.direita.balanco() < 0:
+				self.rotacaoEsquerda()
+			else:
+				self.rotacaoDireitaEsquerda()
+
+	def insere(self,data):
+		if data <= self.data:
+			if not self.esquerda:
+				self.esquerda = avlTree(data)
+			else:
+				self.esquerda.insere(data)
+		else:
+			if not self.direita:
+				self.direita = avlTree(data)
+			else:
+				self.direita.insere(data)
+		self.executaBalanco()
